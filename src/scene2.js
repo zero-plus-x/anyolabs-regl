5c9589071f94718f104d6d2c25bd1bfaebcf4ca6
Nikita Dudnik (nek) <nikdudnik@gmail.com>
2025-03-20 19:34:31 +0100

    Create "water" blob

--- src/scene2.js
+++ src/scene2.js
@@ -5,9 +5,8 @@ import { default as icosphere } from 'icosphere'
 import { default as sphere } from 'primitive-sphere'
 import normals from 'angle-normals'
 import mc from 'mouse-change'
-const teapot = icosphere(4)
+const teapot = icosphere(5)
 const background = sphere(20, { segments: 32 })
-console.log(background)
 const mouse = mc()
 
 const regl = initRegl()
@@ -15,7 +14,7 @@ const CUBE_MAP_SIZE = 512
 
 const GROUND_TILES = 20
 const GROUND_HEIGHT = -5.0
-const TEAPOT_TINT = [0.9, 1.0, 0.8]
+const TEAPOT_TINT = [1, 1, 1]
 
 const teapotFBO = regl.framebufferCube(CUBE_MAP_SIZE)
 
@@ -99,10 +98,103 @@ const vertexShader = /*glsl*/ `
   attribute vec3 position, normal;
   uniform mat4 projection, view, model;
   uniform vec3 eye;
+  uniform float iTime;
   varying vec3 eyeDir, fragNormal;
 
+  // Permutation function
+vec3 mod289(vec3 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 mod289(vec4 x) {
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x) {
+  return mod289(((x * 34.0) + 1.0) * x);
+}
+
+vec4 taylorInvSqrt(vec4 r) {
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+// Simplex noise function
+float snoise(vec3 v) {
+  const vec2 C = vec2(1.0/6.0, 1.0/3.0);
+  const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);
+
+  // First corner
+  vec3 i  = floor(v + dot(v, C.yyy));
+  vec3 x0 = v - i + dot(i, C.xxx);
+
+  // Other corners
+  vec3 g = step(x0.yzx, x0.xyz);
+  vec3 l = 1.0 - g;
+  vec3 i1 = min(g.xyz, l.zxy);
+  vec3 i2 = max(g.xyz, l.zxy);
+
+  // Offsets for other corners
+  vec3 x1 = x0 - i1 + C.xxx;
+  vec3 x2 = x0 - i2 + C.yyy;
+  vec3 x3 = x0 - D.yyy;
+
+  // Permutations
+  i = mod289(i);
+  vec4 p = permute(permute(permute(
+              i.z + vec4(0.0, i1.z, i2.z, 1.0))
+            + i.y + vec4(0.0, i1.y, i2.y, 1.0))
+            + i.x + vec4(0.0, i1.x, i2.x, 1.0));
+
+  // Gradients:  ( x|y|z 1 ) * ( 1|2+z|2-y )
+  float n_ = 1.0 / 7.0; // 1.0 / 7.0
+  vec3 ns = n_ * D.wyz - D.xzx;
+
+  vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  // mod(p,7*7)
+
+  vec4 x_ = floor(j * ns.z);
+  vec4 y_ = floor(j - 7.0 * x_); // mod(j,N)
+
+  vec4 x = x_ * ns.x + ns.yyyy;
+  vec4 y = y_ * ns.x + ns.yyyy;
+  vec4 h = 1.0 - abs(x) - abs(y);
+
+  vec4 b0 = vec4(x.xy, y.xy);
+  vec4 b1 = vec4(x.zw, y.zw);
+
+  vec4 s0 = floor(b0) * 2.0 + 1.0;
+  vec4 s1 = floor(b1) * 2.0 + 1.0;
+  vec4 sh = -step(h, vec4(0.0));
+
+  vec4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
+  vec4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
+
+  vec3 g0 = vec3(a0.xy, h.x);
+  vec3 g1 = vec3(a0.zw, h.y);
+  vec3 g2 = vec3(a1.xy, h.z);
+  vec3 g3 = vec3(a1.zw, h.w);
+
+  // Normalization
+  vec4 norm = taylorInvSqrt(vec4(dot(g0, g0), dot(g1, g1), dot(g2, g2), dot(g3, g3)));
+  g0 *= norm.x;
+  g1 *= norm.y;
+  g2 *= norm.z;
+  g3 *= norm.w;
+
+  // Compute noise contributions from each corner
+  vec4 m = max(0.6 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
+  m = m * m;
+  return 42.0 * dot(m * m, vec4(dot(g0, x0), dot(g1, x1), dot(g2, x2), dot(g3, x3)));
+}
+
   void main () {
-    vec4 worldPos = model * vec4(position, 1);
+    vec3 seed = position * 0.2;
+    seed.z += sin(iTime) * 0.5 + 0.5;
+    vec3 seed2 = position * 0.5;
+    seed2.z += sin(iTime * 2.0 + 100.);
+    vec3 pos = position;
+    pos += normal * snoise(seed) * 0.2;
+    pos += normal * snoise(seed2) * 0.1;
+    vec4 worldPos = model * vec4(pos, 1);
     vec4 worldNormal = model * vec4(normal, 0);
 
     fragNormal = normalize(worldNormal.xyz);
@@ -120,7 +212,7 @@ const drawTeapot = regl({
 
   void main () {
     vec4 env = textureCube(envMap, reflect(-eyeDir, fragNormal));
-    gl_FragColor = vec4(env.rgb * (normalize(fragNormal) + 0.8), 1);
+    gl_FragColor = vec4(env.rgb, 1);
   }`,
 
   vert: vertexShader,
@@ -142,6 +234,7 @@ const drawTeapot = regl({
     tint: regl.prop('tint'),
     envMap: teapotFBO,
     model: (context, { position }) => mat4.translate([], mat4.identity([]), position),
+    iTime: ({ tick }) => 0.01 * tick,
   }
 })
 
@@ -178,7 +271,7 @@ void main ()
   // gl_FragColor = vec4(1., 0., 0., 1.);
 
 
-	float time = iTime * .5+23.0;
+	float time = iTime * .1+23.0;
     // uv should be the 0-1 uv of texture...    
 #ifdef SHOW_TILING
 	vec2 p = mod(uv*TAU*2.0, TAU)-250.0;
@@ -280,7 +373,7 @@ regl.frame(({ tick, drawingBufferWidth, drawingBufferHeight, pixelRatio }) => {
     target: [0, 0, 0]
   }, ({ eye, tick }) => {
     regl.clear({
-      color: [0, 0, 0, 1],
+      color: [1, 1, 1, 1],
       depth: 1
     })
     drawGround({
