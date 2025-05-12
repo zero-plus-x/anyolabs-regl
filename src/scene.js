2680c1f3568fce886b39e98d6b827fdc5ec765a4
Nikita Dudnik (nek) <nikdudnik@gmail.com>
2025-05-12 18:51:42 +0200

    Clean up

--- src/scene.js
+++ src/scene.js
@@ -7,20 +7,197 @@ import createSphere from 'primitive-sphere'
 import normals from 'angle-normals'
 import sphereVert from './shaders/glass.vert'
 import sphereFrag from './shaders/glass.frag'
-import bgFrag from './shaders/bg.frag'
-import bgVert from './shaders/bg.vert'
 import * as dat from 'dat.gui'
-import { createSeededRandom, nextPowerOf2, resizeRegl } from './utils'
+import {
+  createSeededRandom,
+  nextPowerOf2,
+  resizeRegl,
+  generateXYGridWithRandomZ,
+  hsvToRgb,
+  generateHueVariants,
+} from './utils'
 
 const sphere = createSphere(1, { segments: 128 })
-const background = createSphere(100, { segments: 64 })
 
 const rand = createSeededRandom(9)
 
 const canvas = document.getElementById('heroImage')
-const regl = createREGL({ canvas, extensions: ['angle_instanced_arrays'] })
+const regl = createREGL({
+  canvas,
+  extensions: ['angle_instanced_arrays'],
+  onDone: (err, regl) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    resizeRegl(canvas, regl, updateFBO)
 
-resizeRegl(canvas, regl, updateFBO)
+    function hexColorToRgb(hex) {
+      const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+      return result
+        ? [parseInt(result[1], 16) / 255, parseInt(result[2], 16) / 255, parseInt(result[3], 16) / 255]
+        : null
+    }
+
+    const colorPoints = [
+      { position: [0, 0], color: hexColorToRgb('#9670c2') },
+      { position: [1, 0], color: hexColorToRgb('#358fe8') },
+      { position: [0, 1], color: hexColorToRgb('#ffffff') },
+      { position: [1, 1], color: hexColorToRgb('#9670c2') },
+      { position: [0.5, 0.5], color: hexColorToRgb('#358fe8') },
+    ]
+
+    const drawAnimatedBackground = regl({
+      vert: `
+          precision mediump float;
+          attribute vec2 position;
+          varying vec2 v_uv;
+          void main() {
+            v_uv = 0.5 * (position + 1.0);
+            gl_Position = vec4(position, 0, 1);
+          }
+        `,
+      frag: /* glsl */ `
+          precision mediump float;
+  
+          uniform vec2 u_positions[5];
+          uniform vec3 u_colors[5];
+          uniform float u_time;
+          varying vec2 v_uv;
+  
+          void main() {
+            vec3 color = vec3(0.0);
+            float totalWeight = 0.0;
+  
+            for (int i = 0; i < 5; i++) {
+              float d = distance(v_uv, u_positions[i] + 0.5 * vec2(sin(u_time + float(i)), cos(u_time + float(i))));
+              float w = exp(-5.0 * d * d); // Gaussian-like falloff
+              color += u_colors[i] * w;
+              totalWeight += w;
+            }
+  
+            color /= totalWeight;
+            gl_FragColor = vec4(color, 1.0);
+        }`,
+      attributes: {
+        position: [
+          [-1, -1],
+          [1, -1],
+          [-1, 1],
+          [-1, 1],
+          [1, -1],
+          [1, 1],
+        ],
+      },
+      uniforms: {
+        'u_positions[0]': () => colorPoints[0].position,
+        'u_positions[1]': () => colorPoints[1].position,
+        'u_positions[2]': () => colorPoints[2].position,
+        'u_positions[3]': () => colorPoints[3].position,
+        'u_positions[4]': () => colorPoints[4].position,
+
+        'u_colors[0]': () => colorPoints[0].color,
+        'u_colors[1]': () => colorPoints[1].color,
+        'u_colors[2]': () => colorPoints[2].color,
+        'u_colors[3]': () => colorPoints[3].color,
+        'u_colors[4]': () => colorPoints[4].color,
+
+        u_time: ({ time }) => time * 0.1,
+      },
+      count: 6,
+      framebuffer: regl.prop('fbo'),
+      depth: {
+        enable: false,
+      },
+    })
+
+    // Dark settings
+    // const settings = {
+    //   "sphere": {
+    //     "reflectionRoughness": 0.07,
+    //     "refractionRoughness": 0.07,
+    //     "refractiveIndex": 2.02,
+    //     "noiseFrequency": 0.195,
+    //     "noiseScale": 1.3,
+    //     "animSpeed": 0.05
+    //   },
+    //   "bg": {
+    //     "animSpeed": 0.1
+    //   }
+    // }
+
+    // Light settings
+    const settings = {
+      sphere: {
+        reflectionRoughness: 0.07,
+        refractionRoughness: 0.07,
+        refractiveIndex: 2.02,
+        noiseFrequency: 0.46530000000000005,
+        noiseScale: 0.1,
+        animSpeed: 0.05,
+      },
+      bg: {
+        animSpeed: 0.1,
+      },
+    }
+
+    const buttons = {}
+    buttons['Copy settings'] = () => {
+      navigator.clipboard.writeText(JSON.stringify(settings, null, 2))
+    }
+
+    const gui = new dat.GUI()
+
+    // gui.add(settings.sphere, 'refractionRoughness').min(0.01).max(1).step(0.01);
+    // gui.add(settings.sphere, 'refractiveIndex').min(1).max(2.33).step(0.01);
+    // gui.add(settings.sphere, 'reflectionRoughness').min(0.01).max(1).step(0.01);
+    gui.add(settings.sphere, 'noiseFrequency').min(0.0025).max(1).step(0.0001)
+    gui.add(settings.sphere, 'noiseScale').min(0.1).max(2).step(0.1)
+    gui.add(settings.sphere, 'animSpeed').min(0.025).max(1).step(0.05).name('Spheres speed')
+    gui.add(settings.bg, 'animSpeed').min(0.025).max(1).step(0.05).name('Bg speed')
+
+    gui.add(buttons, 'Copy settings')
+
+    regl.frame(({ drawingBufferWidth, drawingBufferHeight, pixelRatio }) => {
+      setupCamera(
+        {
+          cameraPosition: [1.5, 2, 15],
+          target: [0, 0, 0],
+        },
+        ({ tick, time }) => {
+          // console.log(tick, time)
+          regl.clear({
+            color: [1, 1, 1, 1],
+            depth: 1,
+          })
+          drawAnimatedBackground({
+            fbo: null,
+          })
+          drawAnimatedBackground({
+            fbo: bgFbo,
+          })
+
+          let newOffset = [...offset.map((p) => [...p])]
+          for (let i = 0; i < offset.length; i++) {
+            const sinForZ = Math.sin(time * 0.01 + (i / offset.length - 0.5) * Math.PI * 13) * 10
+            const z = offset[i][2] + sinForZ
+            newOffset[i][2] = z
+          }
+
+          offsetBuffer.subdata(newOffset)
+
+          drawSphere([
+            {
+              position: [0, 0, 0],
+              envMap: bgFbo,
+              ...settings.sphere,
+            },
+          ])
+        },
+      )
+    })
+  },
+})
 
 const CUBE_MAP_SIZE = 512
 
@@ -87,22 +264,8 @@ const setupCamera = regl({
 const N = 6
 const TOTAL = N * N
 
-function generateXYGridWithRandomZ(N) {
-  const offset = (N - 1) / 2
-  const grid = []
-
-  for (let x = 0; x < N; x++) {
-    for (let y = 0; y < N; y++) {
-      const z = (rand() - 0.5) * 2
-      grid.push([x - offset, y - offset, z])
-    }
-  }
-
-  return grid
-}
-
 const SCALE = 3.5
-const offset = generateXYGridWithRandomZ(N).map((p) => [p[0] * SCALE, p[1] * SCALE, p[2] * 15])
+const offset = generateXYGridWithRandomZ(N, rand).map((p) => [p[0] * SCALE, p[1] * SCALE, p[2] * 15])
 
 const offsetBuffer = regl.buffer({
   length: TOTAL * 3 * 4,
@@ -110,37 +273,9 @@ const offsetBuffer = regl.buffer({
   usage: 'dynamic',
 })
 
-function hsvToRgb({ h, s, v }) {
-  let c = v * s
-  let x = c * (1 - Math.abs(((h / 60) % 2) - 1))
-  let m = v - c
-  let [r, g, b] = [0, 0, 0]
-
-  if (h < 60) [r, g, b] = [c, x, 0]
-  else if (h < 120) [r, g, b] = [x, c, 0]
-  else if (h < 180) [r, g, b] = [0, c, x]
-  else if (h < 240) [r, g, b] = [0, x, c]
-  else if (h < 300) [r, g, b] = [x, 0, c]
-  else [r, g, b] = [c, 0, x]
-
-  return [Math.round((r + m) * 255), Math.round((g + m) * 255), Math.round((b + m) * 255)]
-}
-function generateHueVariants(baseHSV, count, hueOffset = 30, seededRand = Math.random) {
-  const { h: baseH, s, v } = baseHSV
-  const variants = []
-
-  for (let i = 0; i < count; i++) {
-    const offset = (seededRand() - 0.5) * 2 * hueOffset
-    let h = (baseH + offset) % 360
-    if (h < 0) h += 360
-    variants.push({ h, s, v })
-  }
-
-  return variants
-}
 const base = { h: 200, s: 0.5, v: 0.01 }
 const variants = generateHueVariants(base, TOTAL)
-console.log(variants)
+
 const drawSphere = regl({
   vert: sphereVert,
   frag: sphereFrag,
@@ -205,8 +340,6 @@ const drawSphere = regl({
   },
 })
 
-const sphereFbo = regl.framebufferCube(CUBE_MAP_SIZE)
-
 const width = nextPowerOf2(canvas.clientWidth)
 const height = nextPowerOf2(canvas.clientHeight)
 const bgFbo = regl.framebuffer({
@@ -221,281 +354,3 @@ const bgFbo = regl.framebuffer({
 function updateFBO(width, height) {
   bgFbo.resize(width, height)
 }
-
-resl({
-  manifest: {
-    palette: {
-      type: 'image',
-      src: 'assets/palette5.png',
-    },
-    bgr: {
-      type: 'image',
-      src: 'assets/background1.jpg',
-    },
-  },
-
-  onDone: ({ palette, bgr }) => {
-    const paletteTexture = regl.texture(palette)
-    const bgrTexture = regl.texture(bgr)
-
-    function createPlane(position = [0, 0, 0], scale = [1, 1, 1]) {
-      const positions = [
-        [-1, -1, 0],
-        [1, -1, 0],
-        [1, 1, 0],
-        [-1, 1, 0],
-      ]
-        .map((p) => vec3.add([], p, position))
-        .map((p) => vec3.scale([], p, scale))
-      const cells = [
-        [0, 1, 2],
-        [2, 3, 0],
-      ]
-      const uvs = [
-        [0, 0],
-        [1, 0],
-        [1, 1],
-        [0, 1],
-      ]
-      return { positions, cells, uvs }
-    }
-    const background = createPlane([0, 0, -29], [100, 100, 1])
-    const bgrFbo = regl.framebuffer({
-      color: regl.texture({ width, height }),
-      depth: true,
-    })
-
-    function hexColorToRgb(hex) {
-      const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-      return result
-        ? [parseInt(result[1], 16) / 255, parseInt(result[2], 16) / 255, parseInt(result[3], 16) / 255]
-        : null
-    }
-
-    const colorPoints = [
-      { position: [0, 0], color: hexColorToRgb('#9670c2') },
-      { position: [1, 0], color: hexColorToRgb('#358fe8') },
-      { position: [0, 1], color: hexColorToRgb('#ffffff') },
-      { position: [1, 1], color: hexColorToRgb('#9670c2') },
-      { position: [0.5, 0.5], color: hexColorToRgb('#358fe8') },
-    ]
-
-    const drawAnimatedBackground = regl({
-      vert: `
-        precision mediump float;
-        attribute vec2 position;
-        varying vec2 v_uv;
-        void main() {
-          v_uv = 0.5 * (position + 1.0);
-          gl_Position = vec4(position, 0, 1);
-        }
-      `,
-      frag: /* glsl */ `
-        precision mediump float;
-
-        uniform vec2 u_positions[5];
-        uniform vec3 u_colors[5];
-        uniform float u_time;
-        varying vec2 v_uv;
-
-        void main() {
-          vec3 color = vec3(0.0);
-          float totalWeight = 0.0;
-
-          for (int i = 0; i < 5; i++) {
-            float d = distance(v_uv, u_positions[i] + 0.5 * vec2(sin(u_time + float(i)), cos(u_time + float(i))));
-            float w = exp(-5.0 * d * d); // Gaussian-like falloff
-            color += u_colors[i] * w;
-            totalWeight += w;
-          }
-
-          color /= totalWeight;
-          gl_FragColor = vec4(color, 1.0);
-      }`,
-      attributes: {
-        position: [
-          [-1, -1],
-          [1, -1],
-          [-1, 1],
-          [-1, 1],
-          [1, -1],
-          [1, 1],
-        ],
-      },
-      uniforms: {
-        'u_positions[0]': () => colorPoints[0].position,
-        'u_positions[1]': () => colorPoints[1].position,
-        'u_positions[2]': () => colorPoints[2].position,
-        'u_positions[3]': () => colorPoints[3].position,
-        'u_positions[4]': () => colorPoints[4].position,
-
-        'u_colors[0]': () => colorPoints[0].color,
-        'u_colors[1]': () => colorPoints[1].color,
-        'u_colors[2]': () => colorPoints[2].color,
-        'u_colors[3]': () => colorPoints[3].color,
-        'u_colors[4]': () => colorPoints[4].color,
-
-        u_time: ({ time }) => time,
-      },
-      count: 6,
-      framebuffer: regl.prop('fbo'),
-      depth: {
-        enable: false,
-      },
-    })
-
-    const backgroundConf = {
-      attributes: {
-        position: [
-          [-1, -1],
-          [1, -1],
-          [-1, 1],
-          [-1, 1],
-          [1, -1],
-          [1, 1],
-        ],
-        uv: [
-          [0, 0],
-          [1, 0],
-          [0, 1],
-          [0, 1],
-          [1, 0],
-          [1, 1],
-        ],
-      },
-      uniforms: {
-        bgr: bgrTexture,
-      },
-      count: 6,
-      vert: `
-        precision mediump float;
-        attribute vec2 position;
-        attribute vec2 uv;
-        varying vec2 v_uv;
-    
-        void main() {
-          v_uv = uv;
-          gl_Position = vec4(position, 0, 1);
-        }
-      `,
-      frag: `
-        uniform sampler2D bgr;
-        precision mediump float;
-        varying vec2 v_uv;
-    
-        void main() {
-          gl_FragColor = texture2D(bgr, v_uv);
-        }
-      `,
-      primitive: 'triangles',
-      depth: {
-        enable: false,
-      },
-    }
-    const drawBackground = regl({
-      ...backgroundConf,
-      framebuffer: regl.prop('fbo'),
-    })
-
-    // Dark settings
-    // const settings = {
-    //   "sphere": {
-    //     "reflectionRoughness": 0.07,
-    //     "refractionRoughness": 0.07,
-    //     "refractiveIndex": 2.02,
-    //     "noiseFrequency": 0.195,
-    //     "noiseScale": 1.3,
-    //     "animSpeed": 0.05
-    //   },
-    //   "bg": {
-    //     "animSpeed": 0.1
-    //   }
-    // }
-
-    // Light settings
-    const settings = {
-      sphere: {
-        reflectionRoughness: 0.07,
-        refractionRoughness: 0.07,
-        refractiveIndex: 2.02,
-        noiseFrequency: 0.46530000000000005,
-        noiseScale: 0.1,
-        animSpeed: 0.05,
-      },
-      bg: {
-        animSpeed: 0.1,
-      },
-    }
-
-    const buttons = {}
-    buttons['Copy settings'] = () => {
-      navigator.clipboard.writeText(JSON.stringify(settings, null, 2))
-    }
-
-    const gui = new dat.GUI()
-
-    // gui.add(settings.sphere, 'refractionRoughness').min(0.01).max(1).step(0.01);
-    // gui.add(settings.sphere, 'refractiveIndex').min(1).max(2.33).step(0.01);
-    // gui.add(settings.sphere, 'reflectionRoughness').min(0.01).max(1).step(0.01);
-    gui.add(settings.sphere, 'noiseFrequency').min(0.0025).max(1).step(0.0001)
-    gui.add(settings.sphere, 'noiseScale').min(0.1).max(2).step(0.1)
-    gui.add(settings.sphere, 'animSpeed').min(0.025).max(1).step(0.05).name('Spheres speed')
-    gui.add(settings.bg, 'animSpeed').min(0.025).max(1).step(0.05).name('Bg speed')
-
-    gui.add(buttons, 'Copy settings')
-
-    regl.frame(({ drawingBufferWidth, drawingBufferHeight, pixelRatio }) => {
-      // render sphere cube map
-      setupCube({ fbo: sphereFbo, center: [0, 0, 0] }, () => {
-        regl.clear({ color: [0.2, 0.2, 0.2, 1], depth: 1 })
-        drawAnimatedBackground({
-          fbo: null,
-        })
-      })
-
-      setupCamera(
-        {
-          cameraPosition: [1.5, 2, 15],
-          target: [0, 0, 0],
-        },
-        ({ tick, time }) => {
-          // console.log(tick, time)
-          regl.clear({
-            color: [1, 1, 1, 1],
-            depth: 1,
-          })
-          drawAnimatedBackground({
-            fbo: null,
-          })
-          drawAnimatedBackground({
-            fbo: bgFbo,
-          })
-
-          let newOffset = [...offset.map((p) => [...p])]
-          for (let i = 0; i < offset.length; i++) {
-            const sinForZ = Math.sin(time * 0.01 + (i / offset.length - 0.5) * Math.PI * 13) * 10
-            const z = offset[i][2] + sinForZ
-            newOffset[i][2] = z
-          }
-
-          offsetBuffer.subdata(newOffset)
-
-          drawSphere([
-            {
-              position: [0, 0, 0],
-              envMap: bgFbo,
-              ...settings.sphere,
-            },
-          ])
-        },
-      )
-    })
-  },
-
-  onProgress: (fraction) => {
-    const intensity = 1.0 - fraction
-    regl.clear({
-      color: [intensity, intensity, intensity, 1],
-    })
-  },
-})
--- src/utils.js
+++ src/utils.js
@@ -32,3 +32,47 @@ export function resizeRegl(canvas, regl, updateFBO) {
 
   resizeObserver.observe(canvas)
 }
+
+export function generateXYGridWithRandomZ(N, rand) {
+  const offset = (N - 1) / 2
+  const grid = []
+
+  for (let x = 0; x < N; x++) {
+    for (let y = 0; y < N; y++) {
+      const z = (rand() - 0.5) * 2
+      grid.push([x - offset, y - offset, z])
+    }
+  }
+
+  return grid
+}
+
+export function hsvToRgb({ h, s, v }) {
+  let c = v * s
+  let x = c * (1 - Math.abs(((h / 60) % 2) - 1))
+  let m = v - c
+  let [r, g, b] = [0, 0, 0]
+
+  if (h < 60) [r, g, b] = [c, x, 0]
+  else if (h < 120) [r, g, b] = [x, c, 0]
+  else if (h < 180) [r, g, b] = [0, c, x]
+  else if (h < 240) [r, g, b] = [0, x, c]
+  else if (h < 300) [r, g, b] = [x, 0, c]
+  else [r, g, b] = [c, 0, x]
+
+  return [Math.round((r + m) * 255), Math.round((g + m) * 255), Math.round((b + m) * 255)]
+}
+
+export function generateHueVariants(baseHSV, count, hueOffset = 30, seededRand = Math.random) {
+  const { h: baseH, s, v } = baseHSV
+  const variants = []
+
+  for (let i = 0; i < count; i++) {
+    const offset = (seededRand() - 0.5) * 2 * hueOffset
+    let h = (baseH + offset) % 360
+    if (h < 0) h += 360
+    variants.push({ h, s, v })
+  }
+
+  return variants
+}
